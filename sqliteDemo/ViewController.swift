6f53a7bcd008b9f1391f9ff653dcff43d6290c64
Agust Lofianto <xiang@agusts-mac.local>
2016-05-20 21:27:22 +0700

    Initial Commit

---
+++ sqliteDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sqliteDemo
+//
+//  Created by Agust Lofianto on 5/20/16.
+//  Copyright © 2016 xiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
